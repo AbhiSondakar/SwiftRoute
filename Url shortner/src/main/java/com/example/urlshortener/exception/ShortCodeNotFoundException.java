8c98260ef79b04f71dcc164d15fd81b139e74723
Abhishek  Sondakar <174420451+AbhiSondakar@users.noreply.github.com>
2026-08-29 01:30:08 +0530

    Delete Url shortner directory

--- Url shortner/src/main/java/com/example/urlshortener/exception/ShortCodeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.urlshortener.exception;
-
-public class ShortCodeNotFoundException extends RuntimeException {
-    public ShortCodeNotFoundException(String shortCode) {
-        super("Short code not found: " + shortCode);
-    }
-}
